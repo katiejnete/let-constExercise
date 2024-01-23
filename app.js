edcab75713bb908d35ee016a18cb001713cc2219
Kate Vong <katiejnete@gmail.com>
2024-01-23 13:58:13 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+// The difference between var and let is var allows redeclaration of a variable, but let doesn't allow that. var has global scoping and function scoping, but let includes more than that, includes block scoping. You can hoist variables with var.
+// The difference between var and const is var allows redeclaration and reassignment of variables. Similarly, var doesn't have block scoping. You can hoist variables with var.
+// The difference between let and const is let allows reassignment of variables.
+// Hoisting is when code is compiled, the variable declaration runs before initialization. It returns the variable as undefined when this happens.
